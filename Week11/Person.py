50a9d1b4f893084ec6a860729cc066e167b1c254
Srushti Patel <srushtimpatel007@gmail.com>
2025-03-23 17:52:44 -0400

    Lab 11

--- Week11/Person.py
+++ Week11/Person.py
@@ -1,24 +1,23 @@
 class Person:
     def __init__(self, p_name, p_age, p_height):
-        print("Constructing the Person Object")
+        print("Constructing the person object")
         self.__name = p_name
         self.__age = p_age
         self.__height = p_height
-        self.public__prop = "I'm a Public Prop"
+        self.public_prop = "I am a public prop"
 
     @property
     def name(self):
-        return self.___name
-    
+        return self.__name
+
     @name.setter
     def name(self, new_name):
         self.__name = new_name
 
     def __del__(self):
-        print("This garbage collector will destroy everything in the end.")
+        print("This garbage collector will destroy everything in the end")
 
-
-    person1 = Person("Srushti", 21, 356)
-    print("The name of the person is: " + str(person1.name))
-    person1.name = "Alfred"
-    print("The name of the person is: " + str(person1.name))
+person1 = Person("Srushti Patel", 60, 176)
+print("The name of the person is: " + str(person1.name))
+person1.name = "Alfred"
+print("The name of the person is now: " + str(person1.name))
